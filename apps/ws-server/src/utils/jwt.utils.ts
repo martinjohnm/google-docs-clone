9ece0ecbd619f7b838f80e14dd3413986c77b4fd
Martin John M <johnmmartin74@gmail.com>
2026-01-21 08:42:24 +0100

    removed jwt.utils.signin function

--- apps/ws-server/src/utils/jwt.utils.ts
+++ apps/ws-server/src/utils/jwt.utils.ts
@@ -2,29 +2,12 @@
 import jwt from "jsonwebtoken"
 const jwt_sec = process.env.JWT_SECRET || "sec"
 
-interface UserToSign {
-    id: string;
-    name: string | null;
-    email: string;
-}
-
 
 interface JwtTokenDecoed {
     id : string,
     name : string | null,
     email : string
 }
-
-export function signJwtToken(user : UserToSign) : string {
-    const name = user.name || "guest"
-    const token = jwt.sign(
-        { id : user.id, name , email : user.email },
-        jwt_sec
-    )
-
-    return token
-}
-
 
 export function verifyJwtToken(token : string) : JwtTokenDecoed {
     return jwt.verify(token, jwt_sec) as JwtTokenDecoed
