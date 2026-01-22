2e4530f2a823e85aac4176bcf776b38ae13869f4
Martin John M <johnmmartin74@gmail.com>
2026-01-22 15:19:30 +0100

    Created a seperate user Id for the Db Id in the User class

--- apps/ws-server/src/websocket/SocketManager.ts
+++ apps/ws-server/src/websocket/SocketManager.ts
@@ -6,12 +6,16 @@ import { verifyJwtToken } from "../utils/jwt.utils";
 
 export class User {
     public socket : WebSocket
+    // unique id for the user in the room 
     public id : string
+    // id of the user from Database
+    public userIdFromDb : string
 
     constructor(token : string, socket : WebSocket) {
         const jwtDecoded = verifyJwtToken(token)
         this.socket = socket
-        this.id = jwtDecoded.id;
+        this.userIdFromDb = jwtDecoded.id;
+        this.id = randomUUID()
     }
 }
 
